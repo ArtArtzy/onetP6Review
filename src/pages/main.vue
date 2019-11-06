107e5a8212b00396dee8cb0364883a0d7eaef632
kong <kong>
2019-11-07 03:29:51 +0700

    main mobile

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,66 +1,127 @@
 <template>
-  <div class="bgImage">
-    <div class="mainbox bg3 absolute-center">
-      <div class="row q-pt-xl">
-        <div class="col-5 relative-position q-pl-md">
-          <div class="q-pa-md q-pl-xl">
-            <img style="width:400px;" src="../statics/image/main/PC.png" />
-          </div>
-          <div class="absolute-center text-center" style="width:100%">
-            <div style="height:90px"></div>
-            <div class="text-h4">{{studentData.name}}</div>
+  <div>
+    <div class="desktop-only bgImage">
+      <div class="mainbox bg3 absolute-center">
+        <div class="row q-pt-xl">
+          <div class="col-5 relative-position q-pl-md">
+            <div class="q-pa-md q-pl-xl">
+              <img style="width:400px;" src="../statics/image/main/PC.png" />
+            </div>
+            <div class="absolute-center text-center" style="width:100%">
+              <div style="height:90px"></div>
+              <div class="text-h4">{{studentData.name}}</div>
 
-            <div>
-              <br />
-              <div class="text-h4">คะแนนรวม</div>
-              <br />
-              <div class="text-h3">{{totalScore}}</div>
+              <div>
+                <br />
+                <div class="text-h4">คะแนนรวม</div>
+                <br />
+                <div class="text-h3">{{totalScore}}</div>
+              </div>
+            </div>
+          </div>
+          <div class="col-7 q-px-md">
+            <div class="box-menu">
+              <div class="row">
+                <div class="col-12 q-pa-sm" v-for="i in 8" :key="i">
+                  <q-btn
+                    rounded
+                    style="height:120px;width:100%"
+                    class="btn bg-white text-h5"
+                    @click="reviewPageBtn(i)"
+                  >
+                    <div class="row" style="width:100%;height:100px">
+                      <div class="col-2">
+                        <div>
+                          <img
+                            style="width:100px;height:100px"
+                            :src="'../statics/image/main/'+i+'.png'"
+                          />
+                        </div>
+                      </div>
+                      <div class="col-7 text-left q-pt-sm">
+                        <div class="q-pt-lg q-px-md">ทบทวนครั้งที่ {{i}} (คาบ 1 - {{i}})</div>
+                      </div>
+                      <div class="col-3">
+                        <div class="q-pt-lg">
+                          <q-icon
+                            name="fas fa-star"
+                            v-if="scoreData[i-1] >= star1"
+                            class="q-ma-xs color1"
+                          />
+                          <q-icon
+                            name="fas fa-star"
+                            v-if="scoreData[i-1] >= star2"
+                            class="q-ma-xs color1"
+                          />
+                          <q-icon
+                            name="fas fa-star"
+                            v-if="scoreData[i-1] >= star3"
+                            class="q-ma-xs color1"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </q-btn>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <div class="col-7 q-px-md">
-          <div class="box-menu">
+      </div>
+    </div>
+    <div class="mobile-only bgImage">
+      <div>
+        <div class="relative-position" style="height:70px">
+          <div class="absolute-center text-white textTitle">หน้าหลัก</div>
+        </div>
+        <div class="bg1 q-px-sm q-pb-md">
+          <div class="q-pa-md text-center textTitle">{{studentData.name}}</div>
+          <div class="bg-white" style="border-radius: 5px;">
+            <div class="text-center">
+              <span class="textTitle">คะแนนรวม</span>
+              <br />
+              <span class="title">{{totalScore}}</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-12" v-for="i in 8" :key="i">
+          <div
+            style=" border-bottom: 1px solid black;"
+            @click="reviewPageBtn(i)"
+            class="bg-white q-pt-xs"
+          >
             <div class="row">
-              <div class="col-12 q-pa-sm" v-for="i in 8" :key="i">
-                <q-btn
-                  rounded
-                  style="height:120px;width:100%"
-                  class="btn bg-white text-h5"
-                  @click="reviewPageBtn(i)"
-                >
-                  <div class="row" style="width:100%;height:100px">
-                    <div class="col-2">
-                      <div>
-                        <img
-                          style="width:100px;height:100px"
-                          :src="'../statics/image/main/'+i+'.png'"
-                        />
-                      </div>
-                    </div>
-                    <div class="col-7 text-left q-pt-sm">
-                      <div class="q-pt-lg q-px-md">ทบทวนครั้งที่ {{i}} (คาบ 1 - {{i}})</div>
-                    </div>
-                    <div class="col-3">
-                      <div class="q-pt-lg">
-                        <q-icon
-                          name="fas fa-star"
-                          v-if="scoreData[i-1] >= star1"
-                          class="q-ma-xs color1"
-                        />
-                        <q-icon
-                          name="fas fa-star"
-                          v-if="scoreData[i-1] >= star2"
-                          class="q-ma-xs color1"
-                        />
-                        <q-icon
-                          name="fas fa-star"
-                          v-if="scoreData[i-1] >= star3"
-                          class="q-ma-xs color1"
-                        />
-                      </div>
-                    </div>
+              <div class="col-3">
+                <img class="q-pa-sm" style="width:100%" :src="'../statics/image/main/'+i+'.png'" />
+              </div>
+              <div class="col-5 q-pa-sm textTitle">
+                ทบทวนครั้งที่ {{i}}
+                <br />
+                (คาบ 1 - {{i}})
+              </div>
+              <div class="col-4 relative-position">
+                <div class="absolute-center" style="width:100%">
+                  <div class="row q-pb-sm">
+                    <q-icon
+                      size="sm"
+                      name="fas fa-star"
+                      v-if="scoreData[i-1] >= star1"
+                      class="q-ma-xs color1"
+                    />
+                    <q-icon
+                      size="sm"
+                      name="fas fa-star"
+                      v-if="scoreData[i-1] >= star2"
+                      class="q-ma-xs color1"
+                    />
+                    <q-icon
+                      size="sm"
+                      name="fas fa-star"
+                      v-if="scoreData[i-1] >= star3"
+                      class="q-ma-xs color1"
+                    />
                   </div>
-                </q-btn>
+                </div>
               </div>
             </div>
           </div>
@@ -119,6 +180,13 @@ export default {
 </script>
 
 <style  scoped>
+.textTitle {
+  font-size: 5vw;
+}
+
+.title {
+  font-size: 10vw;
+}
 .mainbox {
   border-radius: 5px;
   width: 1200px;

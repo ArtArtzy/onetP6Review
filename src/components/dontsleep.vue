5a731762155b65f8c2098200489330fd413bb497
kong <kong>
2019-11-12 14:04:59 +0700

    แก้รูปหมุน

--- src/components/dontsleep.vue
+++ src/components/dontsleep.vue
@@ -2,7 +2,7 @@
   <div class="col-12 row mobile-only" align="center">
     <div class="absolute-center col-12 bg3 orientation-landscape" style="height:100%">
       <div>
-        <img src="../statics/image/pleseRotate.gif" style=" height:200px" class="brx" />
+        <img src="../statics/image/pleseRotate.gif" style=" height:250px" />
       </div>
       <span class="text-h5">โปรแกรมนี้สามารถใช้ได้เฉพาะหน้าจอแนวตั้ง</span>
     </div>
